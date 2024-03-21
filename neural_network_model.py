ae70999ba656759f133053bc405d34ddfa09016c
Nghia <19204686+nghiamphan@users.noreply.github.com>
2024-03-21 14:05:41 -0300

    add postal code as embedding features for model do more processing to remove outlier and bad data print detail prediction to csv

--- neural_network_model.py
+++ neural_network_model.py
@@ -1,14 +1,39 @@
-import math
 import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from torch.utils.data import DataLoader, Dataset, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset
 
-rent_scaler = MinMaxScaler()
+INPUT_COLUMNS = [
+    "postal_code_first_3_idx",
+    "postal_code_idx",
+    "beds",
+    "baths",
+    "area",
+    "studio",
+    "pet_friendly",
+    "furnished",
+    "fitness_center",
+    "swimming_pool",
+    "recreation_room",
+    "heating",
+    "water",
+    "internet",
+    "ensuite_laundry",
+    "laundry_room",
+    "parking",
+    "underground_parking",
+]
+
+gv_input_scaler = MinMaxScaler(feature_range=(1, 3))
+gv_rent_scaler = MinMaxScaler()
+
+gv_n_postal_codes_first_3 = 0
+gv_n_postal_codes = 0
 
 
 def setup_data():
@@ -32,15 +57,37 @@ def setup_data():
 def process_data():
     df = pd.read_csv("data/units_full_info.csv")
 
+    # df = df[df["city"] == "Ottawa"]
+
+    # Filter apartments
+    df = df[df["property_type"] == "apartment"]
+
     # Process the 'area' column
+    df.loc[df["area"] < 300, "area"] = 0
+
     process_missing_area(df)
+    df = df[(df["area"].notna()) & (df["area"] != 0) & (df["rent"].notna()) & (df["rent"] != 0)]
 
     # Process the "studio" column
     df["studio"] = (df["beds"] == 0).astype(int)
     df.loc[df["beds"] == 0, "beds"] = 1
 
+    # Remove outliers based on the rent-to-area ratio
+    df["rent_to_area"] = df["rent"] / df["area"]
+    df = df[(df["rent_to_area"] > 1) & (df["rent_to_area"] < 4)]
+
+    # Convert postal_code to category and then to its corresponding codes
+    df = df[df["postal_code"].notna()]
+    df["postal_code_first_3_idx"] = df["postal_code"].astype(str).str[:3].astype("category").cat.codes
+    df["postal_code_idx"] = df["postal_code"].astype("category").cat.codes
+
+    global gv_n_postal_codes_first_3
+    global gv_n_postal_codes
+    gv_n_postal_codes_first_3 = df["postal_code_first_3_idx"].nunique()
+    gv_n_postal_codes = df["postal_code_idx"].nunique()
+
     # Normalize the 'beds', 'baths' and 'area' columns
-    df[["beds", "baths", "area"]] = MinMaxScaler().fit_transform(df[["beds", "baths", "area"]])
+    df[["beds", "baths", "area"]] = gv_input_scaler.fit_transform(df[["beds", "baths", "area"]])
 
     # Process the 'pet_friendly' column
     df["pet_friendly"] = df[["pet_friendly", "Pet Friendly"]].any(axis=True).astype(int)
@@ -78,37 +125,12 @@ def process_data():
     # Process the 'undergrounnd_parking' column
     df["underground_parking"] = df[["Parking - Underground"]].any(axis=True).astype(int)
 
-    # Filter out rows with missing or 0 values for 'area' and 'rent'
-    df = df[(df["area"].notna()) & (df["area"] != 0) & (df["rent"].notna()) & (df["rent"] != 0)]
-
-    # Filter apartments
-    df = df[df["property_type"] == "apartment"]
-
     print("Dataset size:", len(df))
 
-    input = df[
-        [
-            "beds",
-            "baths",
-            "area",
-            "studio",
-            "pet_friendly",
-            "furnished",
-            "fitness_center",
-            "swimming_pool",
-            "recreation_room",
-            "heating",
-            "water",
-            "internet",
-            "ensuite_laundry",
-            "laundry_room",
-            "parking",
-            "underground_parking",
-        ]
-    ]
+    input = df[INPUT_COLUMNS]
 
     # Normalize the 'rent' column
-    df["rent"] = rent_scaler.fit_transform(df[["rent"]])
+    df["rent"] = gv_rent_scaler.fit_transform(df[["rent"]])
 
     target = df["rent"]
     return input, target
@@ -139,7 +161,15 @@ def process_missing_area(df):
 
 
 class NeuralNetwork(nn.Module):
-    def __init__(self, input_dim, d_ff=64):
+    def __init__(
+        self,
+        input_dim,
+        d_ff=64,
+        postal_code_first_3_dim=4,
+        postal_code_dim=2,
+        n_postal_codes_first_3=1000,
+        n_postal_codes=2000,
+    ):
         super(NeuralNetwork, self).__init__()
 
         if torch.cuda.is_available():
@@ -147,8 +177,11 @@ class NeuralNetwork(nn.Module):
         else:
             self.device = torch.device("cpu")
 
+        self.postal_code_embedding_first_3 = nn.Embedding(n_postal_codes_first_3, postal_code_first_3_dim)
+        self.postal_code_embedding = nn.Embedding(n_postal_codes, postal_code_dim)
+
         self.feed_forward = nn.Sequential(
-            nn.Linear(input_dim, d_ff),
+            nn.Linear(input_dim + postal_code_first_3_dim + postal_code_dim - 2, d_ff),
             nn.ReLU(),
             nn.Linear(d_ff, 1),
         )
@@ -156,12 +189,28 @@ class NeuralNetwork(nn.Module):
         self.to(self.device)
 
     def forward(self, x):
+        # Split the input into the postal code and other features
+        postal_code_first_3 = x[:, 0].long()
+        postal_code = x[:, 1].long()
+        other_features = x[:, 2:]
+
+        # Embed the postal codes
+        postal_code_first_3 = self.postal_code_embedding_first_3(postal_code_first_3)
+        postal_code = self.postal_code_embedding(postal_code)
+
+        # Concatenate the embeddings with the other features
+        x = torch.cat((postal_code_first_3, postal_code, other_features), dim=1)
+
         out = self.feed_forward(x)
         return out
 
 
 def train_and_test(dataloader, input_test, target_test):
-    model = NeuralNetwork(dataloader.dataset.tensors[0].shape[1])
+    model = NeuralNetwork(
+        dataloader.dataset.tensors[0].shape[1],
+        n_postal_codes_first_3=gv_n_postal_codes_first_3,
+        n_postal_codes=gv_n_postal_codes,
+    )
 
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-5)
@@ -173,9 +222,9 @@ def train_and_test(dataloader, input_test, target_test):
             target_batch = target_batch.to(model.device).unsqueeze(1)
 
             optimizer.zero_grad()
-            outputs = model(input_batch)
+            prediction = model(input_batch)
 
-            loss = criterion(outputs, target_batch)
+            loss = criterion(prediction, target_batch)
             loss.backward()
             optimizer.step()
 
@@ -188,17 +237,45 @@ def train_and_test(dataloader, input_test, target_test):
         input_test = input_test.to(model.device)
         target_test = target_test.to(model.device).unsqueeze(1)
 
-        outputs = model(input_test)
-        loss = criterion(outputs, target_test)
+        prediction = model(input_test)
+        loss = criterion(prediction, target_test)
         print(f"\nTest Loss: {loss.item()}")
+        print_result(input_test, target_test, prediction)
 
-    predictions = rent_scaler.inverse_transform(outputs.cpu().numpy().reshape(-1, 1)).flatten()
-    actual = rent_scaler.inverse_transform(target_test.cpu().numpy().reshape(-1, 1)).flatten()
 
-    with open("./data/prediction.txt", "w") as f:
-        f.write("Prediction - Actual - Difference\n")
-        for i in range(len(predictions)):
-            f.write(f"{predictions[i]:<10.2f} {actual[i]:<10.0f} {predictions[i] - actual[i]:.2f}\n")
+def print_result(input_test: torch.Tensor, target_test: torch.Tensor, prediction: torch.Tensor):
+    # Convert tensors to numpy arrays and reshape
+    input_test_np = input_test.cpu().numpy().reshape(-1, input_test.shape[1])
+
+    prediction_np = gv_rent_scaler.inverse_transform(prediction.cpu().numpy().reshape(-1, 1)).flatten()
+    target_test_np = gv_rent_scaler.inverse_transform(target_test.cpu().numpy().reshape(-1, 1)).flatten()
+
+    residual = prediction_np - target_test_np
+
+    df_input_test = pd.DataFrame(input_test_np, columns=INPUT_COLUMNS)
+    df_input_test[["beds", "baths", "area"]] = gv_input_scaler.inverse_transform(
+        df_input_test[["beds", "baths", "area"]]
+    )
+
+    df_target_test = pd.DataFrame(target_test_np, columns=["actual rent"])
+    df_prediction = pd.DataFrame(prediction_np, columns=["prediction"])
+    df_residual = pd.DataFrame(residual, columns=["residual"])
+
+    df = pd.concat([df_input_test, df_target_test, df_prediction, df_residual], axis=1)
+
+    df = df.sort_values(by="residual")
+
+    df.to_csv("data/predictions.csv", index=False)
+
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(target_test_np, residual, alpha=0.5)
+    plt.xlabel("Actual")
+    plt.ylabel("Residual")
+    plt.title("Difference between Predictions and Actual Values")
+
+    # Show the plot
+    plt.show()
 
 
 def main():
